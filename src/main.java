b772b87277176f5544ece1633013b945a93cc76d
leon2 <leon2@192.168.1.7>
2017-07-04 12:15:23 -0600

    nuevo

---
+++ src/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author leon2
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Esto es una pureba");
+        
+        
+        // TODO code application logic here
+    }
+    
+}
